3a5f6ab574c730d0b77cae0940ba8c6fd7db1526
manishrai0021 <94151781+manishrai0021@users.noreply.github.com>
2023-07-13 00:32:27 +0545

    Initial 🚀

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import Nav from './Components/Nav/Nav'
+import React from "react";
+import Nav from "./Components/Nav/Nav";
+import Home from "./Components/Home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Farmers from "./Components/Farmers/Farmers";
+import Login from "./Components/Nav/Login";
+import Signup from "./Components/Nav/Signup";
+import ForgetPass from "./Components/Pages/ForgetPass";
+import AboutUs from "./Components/Home/AboutUs";
+import ContactUs from "./Components/Home/ContactUs";
 
 const App = () => {
   return (
-    <div>
-      <Nav/>
-      <div className='bg-slate-100 h-[100vh] '>Rest</div>
-    </div>
-  )
-}
+    <Router>
+      <div className="App bg-stone-950">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/farmers" element={<Farmers />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/reset" element={<ForgetPass />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import Leaf from '../../Assets/Svgs/leaf.svg'
 
 const Nav = () => {
   return (
     <div className='shadow-lg border-b'>
         <div className='flex justify-between p-4'>
-            <h2 className='text-2xl font-bold text-[2rem] '>Food</h2>
+            <Link to="/" className=' flex gap-1 text-2xl font-bold text-[2rem] text-white'>AgroEco <img src={Leaf} alt="" /> </Link>
             <div className='flex gap-4'>
-                <button className='border-2 rounded-md px-2'>Login</button>
-                <button className='border-2 rounded-md  px-2'>Signup</button>
+                <Link to='/login' className='border-[2px] border-[#0cb01d] rounded-md px-2 text-white hover:bg-[#0cb01d] hover:text-white'>Login</Link>
+                <Link to='/signup' className='border-[2px] rounded-md border-[#0cb01d]  px-2 hover:bg-[#0cb01d] text-white'>Signup</Link>
             </div>
         </div>
     </div>
